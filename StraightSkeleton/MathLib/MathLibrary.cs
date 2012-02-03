f9ce35b8eb6f2bfa042e5da3e16a2b8c40cfc173
Steve Ramusivich <svramusi@gmail.com>
2012-02-02 22:29:13 -0500

    removed unnecessary dependencies. began work on math library

---
+++ StraightSkeleton/MathLib/MathLibrary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using StraightSkeletonLib;
+
+namespace MathLib
+{
+    public class MathLibrary
+    {
+        public static double GetDistanceBetweenVertices(Vertex v1, Vertex v2)
+        {
+            double xDistance = Math.Abs(v1.GetX() - v2.GetX());
+            double yDistance = Math.Abs(v1.GetY() - v2.GetY());
+            double cSquared = (xDistance * xDistance) + (yDistance * yDistance);
+
+            return Math.Sqrt(cSquared);
+        }
+
+        //V2 ALWAYS NEEDS TO BE THE MIDDLE VERTEX
+        public static double GetAngleBetweenVerticese(Vertex v1, Vertex v2, Vertex v3)
+        {
+            double a = MathLibrary.GetDistanceBetweenVertices(v1, v2);
+            double b = MathLibrary.GetDistanceBetweenVertices(v2, v3);
+            double c = MathLibrary.GetDistanceBetweenVertices(v1, v3);
+
+            return RadiansToDegrees(Math.Acos(((a * a) + (b * b) - (c * c)) / (2 * a * b)));
+        }
+
+        private static double RadiansToDegrees(double rad)
+        {
+            return rad * (180.0 / Math.PI);
+        }
+    }
+}
---
+++ StraightSkeleton/MathLibTests/MathLibraryTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using MathLib;
+using StraightSkeletonLib;
+
+using NUnit.Framework;
+
+namespace MathLibTests
+{
+    [TestFixture]
+    public class MathLibraryTests
+    {
+        [Test]
+        public void DistanceBetweenVerticesTests()
+        {
+            Vertex v1;
+            Vertex v2;
+
+            v1 = new Vertex(0, 0);
+            v2 = new Vertex(1, 0);
+            Assert.AreEqual(1.0, MathLibrary.GetDistanceBetweenVertices(v1, v2));
+
+            v1 = new Vertex(0, 0);
+            v2 = new Vertex(2, 0);
+            Assert.AreEqual(2.0, MathLibrary.GetDistanceBetweenVertices(v1, v2));
+
+            v1 = new Vertex(1, 5);
+            v2 = new Vertex(-2, 1);
+            Assert.AreEqual(5, MathLibrary.GetDistanceBetweenVertices(v1, v2));
+
+            v1 = new Vertex(-2, -3);
+            v2 = new Vertex(-4, 4);
+            Assert.AreEqual(7.28011, Math.Round(MathLibrary.GetDistanceBetweenVertices(v1, v2), 5));
+        }
+
+        [Test]
+        public void AngleBetweenVerticesTests()
+        {
+            Vertex v1;
+            Vertex v2;
+            Vertex v3;
+
+            v1 = new Vertex(0, 3);
+            v2 = new Vertex(0, 0);
+            v3 = new Vertex(4, 0);
+
+            Assert.AreEqual(90, MathLibrary.GetAngleBetweenVerticese(v1, v2, v3));
+        }
+    }
+}
